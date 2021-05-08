f6b1071d2b7dd28ccea607ca45423ce98d8ad432
Mohammad Oumeir Rengony <oumeir.rengony@hangarww.com>
2021-05-08 19:16:01 +0400

    Added cartItem to cartDropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
+import {useSelector} from 'react-redux';
+
 import Custombutton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.components';
 
 import './cart-dropdown.styles.scss';
 
 
 const CartDropdown = () => {
+
+    //STATE
+    const {cartItems} = useSelector(state => state.cart);
+
     return(
         <div className="cart-dropdown">
-            <div className="cart-items" />
+            <div className="cart-items">
+                {
+                    cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+                }
+            </div>
             <Custombutton>GO TO CHECKOUT</Custombutton>
         </div>
     );
