03d9bb296e52056e8d274658adb237d83bc8275d
Mohammad Oumeir Rengony <oumeir.rengony@hangarww.com>
2021-05-08 15:20:33 +0400

    cartIcon and cartDropdown with cartReducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import {Link} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 
 import {auth} from '../../firebase/firebase-utils';
 
 import {ReactComponent as Logo} from '../../assets/crown.svg';
+
 import './header.styles.scss';
 
 const Header = () => {
 
     //STATE
     const {currentUser} = useSelector(state => state.user);
+    const {hidden: isCartHidden} = useSelector(state => state.cart);
 
     return(
         <div className="header">
@@ -25,9 +31,17 @@ const Header = () => {
                         (<div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>):
                             <Link className="option" to="/signin">SIGN IN</Link>
                 }
+
+                <CartIcon />
             </div>
+            
+            {
+                isCartHidden ? null : <CartDropdown />
+            }
+            
+            
         </div>
-    )
+    );
 };
 
 export default Header;
